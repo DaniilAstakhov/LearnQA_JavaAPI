9c06c2523abe922be03c707a94c3aa61cb824a81
Даниил Астахов <danya.astahoff@gmail.com>
2024-02-15 20:18:12 +0300

    LongRedirectHomework

---
+++ src/test/java/LongRedirectHomework.java
@@ -0,0 +1,32 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class LongRedirectHomework {
+    @Test
+    public void LongRedirectTest1() {
+        Response response = RestAssured
+                .given()
+                .redirects().follow(false)
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect");
+
+        int statusCode = response.getStatusCode();
+        //System.out.println("Status code: " + statusCode);
+        String redirectLocation = response.getHeader("Location");
+        System.out.println(redirectLocation);
+
+        while (statusCode >= 300 && statusCode < 400){
+            Response newRedirectResponse = RestAssured
+                    .given()
+                    .redirects().follow(false)
+                    .when()
+                    .get(redirectLocation);
+            statusCode = newRedirectResponse.getStatusCode();
+            //System.out.println("Status code: " + statusCode);
+            redirectLocation = newRedirectResponse.getHeader("Location");
+            if (redirectLocation != null)
+            System.out.println(redirectLocation);
+        }
+    }
+}
